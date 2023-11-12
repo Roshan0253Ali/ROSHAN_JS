2dab5b16f2203a0e13e63e46ab6c91696bc8d4fa
Roshan0253Ali <150534991+Roshan0253Ali@users.noreply.github.com>
2023-11-12 17:36:36 +0000

    conversion of data types

---
+++ .devcontainer/03_conversionOperation.js
@@ -0,0 +1,21 @@
+/*
+let score="333aa"//null, undefined,boolean,strings
+console.log( typeof score);
+let valueIntNumber= Number (score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+
+//"33" => 33
+// "331bc" => nan
+//true => 1; false= 0  */
+
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0=>false
+//"roshan"=> true
+
+let someNumber=33
+// converted into strings
