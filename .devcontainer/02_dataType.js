d478b1d31c4390b93822a8f6d00192e98da3e96d
Roshan0253Ali <150534991+Roshan0253Ali@users.noreply.github.com>
2023-11-12 17:17:07 +0000

    datatypes

---
+++ .devcontainer/02_dataType.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all js code as newer version
+//alert(3+3) //we are using nodejs , not browser
+
+let name="roshan"
+let age=16
+let isLoggedIn=false
+let state;
+
+//number
+//bigInt
+//string
+// boolean > true/false
+//null => standalone value
+//undefined
+// symbol=> unique
+
+
+//object
+
+console.log(typeof age)// number
+console.log(typeof null)// object
+console.log(typeof undefined)// undefined
