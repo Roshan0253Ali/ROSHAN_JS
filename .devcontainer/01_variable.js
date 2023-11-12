3dbbebeb77053152c70c9663ca782c650827dcbe
Roshan0253Ali <150534991+Roshan0253Ali@users.noreply.github.com>
2023-11-12 17:13:23 +0000

    datatypes

---
+++ .devcontainer/01_variable.js
@@ -0,0 +1,17 @@
+const accountId=1234
+let=accoutEmail="roshan@gmail"
+var accountPassword="4567"
+accountCity="siwan"
+let accountState;
+
+//accountId=2 // not change const
+
+ accoutEmail="spirit@gmail.com"
+ accountPassword="1212"
+ accountCity="bindusar"
+
+ console.table([accoutEmail,accountPassword,accountCity])
+ /*
+ Prefer not to use var because of issue in block scope and funtion scope
+ */
+console.log(accountState);
